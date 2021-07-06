3f920380c49b462205a3eb3e14171f61e8d90838
dain-lee <dain9934@gmail.com>
2021-07-06 13:58:11 +0900

    📝 1주차 - Java 입출력, 제어문

---
+++ 1주차/Java-flow-control/src/ArrayApp.java
@@ -0,0 +1,22 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		
+		// egoing, jinhuck, youbin 
+//      String users = "egoing, jinhuck, youbin";
+        String[] users = new String[3];
+        users[0] = "egoing";
+        users[1] = "jinhuck";
+        users[2] = "youbin";
+         
+        System.out.println(users[1]); // jinhuck
+        System.out.println(users.length); // 3
+         
+        int[] scores = {10, 100, 100}; // ¿ø¼Ò, element
+        System.out.println(scores[1]); // 100
+        System.out.println(scores.length); // 3
+
+	}
+
+}
